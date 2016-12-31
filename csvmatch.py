e0369159aa76c79f87a68a7ac7b44d0b98c82f65
Mike Perham <mkperham@gmail.com>
2016-12-31 09:20:04 -0500

    added function to not repeat rows

--- csvmatch.py
+++ csvmatch.py
@@ -13,8 +13,8 @@
     this will be done with the aid of stringscore 0.1.0
 """
 
-import csv, pprint
-#from stringscore import liquidmetal
+import csv, pprint, sys
+from datetime import datetime, timedelta
 from fuzzywuzzy import fuzz
 
 # csv_object generates a csv reader object for subsequent copying of contents
@@ -37,14 +37,27 @@ def list_rid(csvfile):
         csv_list.append(row)
     return csv_list
 
+def lose_headers(csvdata):
+    return csvdata[1:]
+
+def convertdates(csvdata, datecolumn):
+    for row in csvdata:
+        #print(row[datecolumn-1])
+        try:
+            row[datecolumn-1]=datetime.strptime(row[datecolumn-1], "%m/%d/%y %H:%M")
+        except:
+            print("NOPE, fucked up!!")
+    return csvdata
+    
+
 # generates scores for each item in data1col1 compared to data1col2 (2x2 matrix)
 def generate_scores(headers, csvdata1, csvdata2, d1colnum, d2colnum):
     allscores = []
-    d1col, d2col = d1colnum-1, d2colnum-1
+    # reset the data column numbers so they fit with python's default zero-index
+    d1col, d2col = int(d1colnum)-1, int(d2colnum)-1
     if headers:
         data1 = csvdata1[1:]
         data2 = csvdata2[1:]
-        pprint.pprint(data1)
     else:
         data1 = csvdata1[:]
         data2 = csvdata[:]
@@ -53,49 +66,82 @@ def generate_scores(headers, csvdata1, csvdata2, d1colnum, d2colnum):
         # get the item in the row for to be matched
         item = row[d1col]
         itemscores = []
-        # iterate over the rows in data file 2
+        # iterate over the rows in data file 2 to find the best match
         for rowcompare in data2:
             # compare the corresponding item in compare row to item in primary row
             # generate a list of scores: primary item <--> compare item in every row
             score = fuzz.ratio(item, rowcompare[d2col])
             itemscores.append(score)
-            print(item, rowcompare[d2col], score)
         # generate a row for comparison scores in an "allscores" list
         allscores.append(itemscores)
     return allscores
 
-def weightscores(scoremat1, scoremat2, weight1, weight2):
-    scoreweight1 = weight1 / (weight1 + weight2)
-    scoreweight2 = weight2 / (weight1 + weight2)
+def datescore(csvdata1, csvdata2, datecol1, datecol2, daylimit):
+    # assumes headerless data
+    datescores = []
+    daylimit = timedelta(days=daylimit)
+    optimal_delta = timedelta(days=3)
+    for row_data1 in csvdata1:
+        singledatescores=[]
+        for row_data2 in csvdata2:
+            td = row_data1[datecol1-1] - row_data2[datecol2-1]
+            if td > daylimit:
+                singledatescores.append(0)
+            elif td < optimal_delta:
+                singledatescores.append(100)
+            else:
+                datescore = 100*(td-optimal_delta)/(daylimit-optimal_delta)
+                singledatescores.append(datescore)
+        datescores.append(singledatescores)
+    return datescores
+
+
+def weightscores(scoremat1, scoremat2, datescores, weight1, weight2, dateweight):
+    # normalize the weights
+    scoreweight1 = weight1 / (weight1 + weight2 + dateweight)
+    scoreweight2 = weight2 / (weight1 + weight2 + dateweight)
+    scoreweight3 = dateweight / (weight1 + weight2 + dateweight)
     scoremat=[]
-    # probably need matrix functionality here
-    tempmat_outer = zip(scoremat1, scoremat2)
+    tempmat_outer = zip(scoremat1, scoremat2, datescores)
     for row in tempmat_outer:
-        temprow = zip(row[0], row[1])
+        temprow = zip(row[0], row[1], row[2])
         scorerow = []
         for cell in temprow:
-            scorerow.append((scoreweight1*cell[0]+scoreweight2*cell[1]))
+            scorerow.append((scoreweight1*cell[0]+
+                             scoreweight2*cell[1]+
+                             scoreweight3*cell[2]))
         scoremat.append(scorerow)           
     return scoremat
 
 def sortscores(scoremat):
     # sort the scores, searching for the highest score per row
+    # output a list of 2-item lists matching the rows from the two data sets
     match_list = []
+    checklist = []
     for mat1idx, row in enumerate(scoremat):
         max_value = max(row)
         max2_index = row.index(max_value)
+        
+        # if max index is already in the matchlist, find the second closest match
+        if max2_index in checklist:
+            second_max = 0
+            for score in row:
+                if score > second_max and score < max_value and (row.index(score) not in checklist):
+                    second_max = score
+            max2_index = row.index(second_max)
+
         match_list.append([mat1idx, max2_index])
+        checklist.append(max2_index)
     return match_list
 
-def makecsvoutdata(matchlist, headers, csvdata1, csvdata2):
+def makecsvoutdata(match_list, headers, csvdata1, csvdata2):
     outdata = []
     indxbump = 0
     if headers:
         headerline = csvdata1[0]+csvdata2[0]
         outdata.append(headerline)
         indxbump = 1
-    for matchrow in matchlist:
-        print(matchrow)
+    for matchrow in match_list:
         linefirst = csvdata1[matchrow[0]+indxbump]
         linesecond = csvdata2[matchrow[1]+indxbump]
         line = linefirst + linesecond
@@ -114,51 +160,61 @@ def writecsv(outdata, outputList):
 
 # main gets the arguments and calls the appropriate functions in order
 def main(*args):
-    '''
-    idx = 0
-    for arg in args:
-        print("arg" + str(idx) + ": " + str(arg))
-        idx+=1
-        print(args[0])
-        print(args[1])
-    '''
     # read arguments from input
     try:
-        dataList1 = args[0] # name of csv 1
-        dataList2 = args[1] # name of csv 2
+
+        # use if calling from script
+        dataList1 = args[0]  # name of csv 1
+        dataList2 = args[1]  # name of csv 2
         outputList = args[2] # name of output file
-        data1col1 = args[3] # first column for first match in csv 1
-        data2col1 = args[4] # first column for first match in csv 2
-        data1col2 = args[5] # second column for second match in csv 1
-        data2col2 = args[6] # second column for second match in csv 2
-        headers = args[7] # boolean if there are headers
+        headers = args[3]    # boolean if there are headers
+        data1col1 = args[4]  # first column for first match in csv 1
+        data2col1 = args[5]  # first column for first match in csv 2
+        data1col2 = args[6]  # second column for second match in csv 1
+        data2col2 = args[7]  # second column for second match in csv 2
+        is_datecol = args[8] # boolean if using dates
+        datecol1 = args[9]   # column number for dates in data1
+        datecol2 = args[10]  # column number for dates in data2
+        
+
     except IndexError:
-        print("something didn't work")
-        '''    
-        dataList1  = "subjectsList.csv"
-        dataList2 = "dataList.csv"
+        print("There was an error with the file names")
+        dataList1  = "test.csv"
+        dataList2 = "test2.csv"
         outputList = "outputList.csv"
-        '''
+
     ### Read rid -- this uses python3 syntax, specifically the "newline" argument
     with open(dataList1 , newline='') as csvfile:
         data1 = list_rid(csvfile)
     with open(dataList2, newline='') as csvfile:
         data2 = list_rid(csvfile)
 
+    ### work without headers
+    if headers:
+        nhdata1 = lose_headers(data1)
+        nhdata2 = lose_headers(data2)
+    else:
+        nhdata1 = data1[:]
+        nhdata2 = data2[:]
+    
+    ### Read and convert dates
+    if is_datecol:
+        nhdata1 = convertdates(nhdata1, datecol1)
+        nhdata2 = convertdates(nhdata2, datecol2)   
+
     ### start the scoring protocol
     col1scores = generate_scores(headers, data1, data2, data1col1, data2col1)
     col2scores = generate_scores(headers, data1, data2, data1col2, data2col2)
-    pprint.pprint(col1scores)
-    pprint.pprint(col2scores)
-    weightedmat = weightscores(col1scores, col2scores, 7, 3)
-    pprint.pprint(weightedmat)
+    datescores = datescore(nhdata1, nhdata2, datecol1, datecol2, daylimit=30)
+    pprint.pprint(datescores)
+    weightedmat = weightscores(col1scores, col2scores, datescores, 1, 3, 20)
     matchlist = sortscores(weightedmat)
-    pprint.pprint(matchlist)
+    print(matchlist)
     outdata = makecsvoutdata(matchlist, headers, data1, data2)
-    print(outdata)
+    #pprint.pprint(outdata)
     writecsv(outdata, outputList)
 
 if __name__=='__main__':
     #main(*sys.argv)
-    main("test.csv", "test2.csv", "test_out.csv", 1,1,2,2, True)
+    main("test.csv", "test2.csv", "test_out.csv", True, 1,1,2,2,1,4,4)
 
